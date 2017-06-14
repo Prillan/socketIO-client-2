d07e385955bed1d75df4f7474e20c40a3ff5e7c3
Rasmus Précenth <rasmus@precenth.eu>
2017-06-14 17:50:11 +0200

    Very ugly.

--- socketIO_client/transports.py
+++ socketIO_client/transports.py
@@ -163,7 +163,10 @@ class WebsocketTransport(AbstractTransport):
         except socket.error as e:
             raise ConnectionError('recv disconnected (%s)' % e)
         if not isinstance(packet_text, six.binary_type):
-            packet_text = six.b(packet_text)
+            try:
+                packet_text = six.b(packet_text)
+            except UnicodeDecodeError:
+                packet_text = packet_text.encode('utf-8')
         engineIO_packet_type, engineIO_packet_data = parse_packet_text(
             packet_text)
         yield engineIO_packet_type, engineIO_packet_data
